e18f414261697e34f168fe1df083a00ff13faef6
luno3301 <113801241+luno3301@users.noreply.github.com>
2024-02-01 21:32:34 +0300

    Add files via upload

---
+++ Новая папка (3)/Новая папка (3)/static/main2.js
@@ -0,0 +1,7 @@
+var a=Math.round(Math.random()*3)
+        image = new Array();
+        image[0]="static/Pc.png"
+        image[1]="static/sistemnik.png"
+        image[2]="static/monik.png"
+        image[3]="static/2321.png"
+        document.write ("<img src=" + image[a] + " class='image'>");   
---
+++ Новая папка (3)/Новая папка (3)/static/main3.js
@@ -0,0 +1,7 @@
+var a=Math.round(Math.random()*3)
+        image = new Array();
+        image[0]="static/pyc.png"
+        image[1]="static/Google.png"
+        image[2]="static/word.png"
+        image[3]="static/firefox.png"
+        document.write ("<img src=" + image[a] + " class='image'>");   
