05c04b1b277a0fc6e16eca1e8721110862d1b43f
luno3301 <113801241+luno3301@users.noreply.github.com>
2024-02-02 09:48:20 +0300

    Delete Новая папка (3)/Новая папка (3) directory

--- Новая папка (3)/Новая папка (3)/static/main.js
+++
@@ -1,9 +0,0 @@
-var a=Math.round(Math.random()*3)
-        image = new Array();
-        image[0]="static/7.png"
-        image[1]="static/2.png"
-        image[2]="static/3.png"
-        image[3]="static/4.png"
-        image[0]="static/6.png"
-        image[1]="static/5.png"
-        document.write ("<img src=" + image[a] + " class='image'>");   
